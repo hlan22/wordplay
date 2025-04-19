0108d5fc8ef6fa11c51618f7ee001dddaf448080
Heidi Lantz <heidilantz22@gmail.com>
2025-04-19 14:24:33 -0700

    update personal code files for better understanding

--- example_wordplay.R
+++ example_wordplay.R
@@ -1,4 +1,4 @@
-# here is a run through of all functions on sample data
+# A run-through of all functions on sample data
 
 # Load necessary libraries
 library(ggplot2)
@@ -6,47 +6,43 @@ library(dplyr)
 library(stringr)
 library(forcats)
 library(stringi)
-# library(tidyr)
 
+# load package
+pak::pak("hlan22/wordplay")
+library(wordplay)
 
-
-words_file <- read.csv("~/Downloads/heidis_list_of_good_words.txt",
-                  header = FALSE,
-                  col.names = c("word", "extra")
-                  )
-mywords <- words_file %>%
-  select(-c(extra))
+mywords <- read.csv("~/Downloads/heidis_good_words.txt",
+                  header = TRUE)
 
 # take a basic look at data
 head(mywords)
+str(mywords)
 summary(mywords)
 
+# use package functions
 all_words_summary(mywords)
 summary(word_characteristics(mywords))
 word_length_plot(mywords)
 top_words_plot(mywords)
 
-
+# Can also try with a lot of other examples!
 dummy_words <- data.frame(word = c("alfa", "bravo", "charlie", "delta"))
 
-nato_alphabet <- data.frame(word = c(
-  "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot",
-  "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima",
-  "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo",
-  "Sierra", "Tango", "Uniform", "Victor", "Whiskey",
-  "X-ray", "Yankee", "Zulu"
-))
+nato_alphabet <- data.frame(
+  word = c(
+    "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf",
+    "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November",
+    "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform",
+    "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"))
 
 greek_alphabet <- data.frame(
   word = c(
     "Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta",
     "Iota", "Kappa", "Lambda", "Mu", "Nu", "Xi", "Omicron", "Pi", "Rho",
-    "Sigma", "Tau", "Upsilon", "Phi", "Chi", "Psi", "Omega"
-  ))
+    "Sigma", "Tau", "Upsilon", "Phi", "Chi", "Psi", "Omega"))
 
+# And test out the functions :)
 all_words_summary(greek_alphabet)
 word_characteristics(greek_alphabet)
 word_length_plot(greek_alphabet)
 top_words_plot(greek_alphabet)
-
-dummy_words <- data.frame(word = c("alfa", "bravo", "charlie", "delta"))
--- package_build_commands.R
+++ package_build_commands.R
@@ -1,76 +1,118 @@
-# all code run in Rstudio terminal to get package working
+# --------------- MAKE A PACKAGE ------------------------------
+# All code run in Rstudio console to make package
 
-library(devtools)
-#> Loading required package: usethis
-
-packageVersion("devtools")
-
-create_package("~/git/wordplay")
-
+# load the useful library that contains usethis
 library(devtools)
 
-use_git()
+# make a package in a new (or existing) directory
+devtools::create_package("~/git/wordplay")
+
+# load again if in new RStudio instance
+library(devtools)
+
+# make package a git repo
+devtools::use_git()
 #> ✔ Initialising Git repo.
-#> ✔ Adding ".Rhistory", ".Rdata", ".httr-oauth", ".DS_Store", and
-#>   ".quarto" to '.gitignore'.
+#> ✔ Adding ".Rhistory", ".Rdata", ".httr-oauth", ".DS_Store", and ".quarto" to '.gitignore'.
 
+# creates files for functions in the R/ folder
+devtools::use_r("all_words_summary")
+devtools::use_r("top_words_plot")
+devtools::use_r("word_characteristics")
+devtools::use_r("word_length_plot")
 
-use_r("all_words_summary")
-use_r("starting_letter_plot")
-use_r("top_words_plot")
-use_r("word_characteristics")
-use_r("word_length_plot")
+# --------------- WRITE YOUR FUNCTIONS ------------------------------
 
-load_all()
+# loads all functions locally
+devtools::load_all()
 
+# checks that functions are not being sourced, that load all works
 exists("all_words_summary", where = globalenv(), inherits = FALSE)
 #> [1] FALSE
 
+# --------------- COMMIT YOUR WORK ------------------------------
 # (in terminal)
 # git remote add origin https://github.com/hlan22/wordplay.git
 # git remove -v # verifies your push/fetch connection
+# ---------------------------------------------------------------
 
-check()
+# ensures package is in working order, gives errors warnings and tests
+devtools::check()
 
-use_mit_license()
+# add a license
+devtools::use_mit_license()
 
 # updated .Rbuildignore
 
-check()
+# make sure updates fix check warnings and errors
+devtools::check()
 
-document()
+# --------------- ADD ROXYGEN STATEMENTS ------------------------------
 
+# add documentation about functions after roxygen statements
+devtools::document()
+
+# now after documenting you can see the ? help for your functions :)
 ?word_length_plot
 
+# re-run to make sure everything is working
 devtools::document()
-
 devtools::check()
 
+# install package
 devtools::install()
 
-use_testthat()
-use_test("all_words_summary")
-use_test("top_words_plot")
-use_test("word_charactersitics")
-use_test("word_length_plot")
-
+# now you are able to use it! minimum viable product
 library(wordplay)
 
-test()
 
+# --------------- PACKAGE USEABLE; ADD MORE ------------------------------
+
+# creates testthat directory, adds to suggested, creates script
+devtools::use_testthat()
+
+# fastest to do this with all R/ function files open
+devtools::use_test()
+
+# however, can create manual testing files too
+devtools::use_test("all_words_summary")
+devtools::use_test("top_words_plot")
+devtools::use_test("word_charactersitics")
+devtools::use_test("word_length_plot")
+
+
+# --------------- WRITE TESTS ------------------------------
+
+# check that your tests are passing
+library(testthat) # i believe is already apart of devtools
+testthat::test()
+
+# re-run and update package based off of tests (tests are ran with check)
 devtools::check()
 devtools::document()
 devtools::install()
 
-load_all()
-test()
-
+# update and check tests
 library(devtools)
+testthat::test()
 
-use_readme_rmd()
+# --------------- ADD MORE INFO ------------------------------
+
+# adds package to imports field of DESCRIPTION
+use_package("ggplot2")
+
+# if renaming functions
+rename_files("function_name", "new_function_name")
+# MUST redo document() if so
+
+# initializa a basic README.Rmd for your github
+devtools::use_readme_rmd()
 #> ✔ Writing 'README.Rmd'.
 #> ✔ Adding "^README\\.Rmd$" to '.Rbuildignore'.
 #> ☐ Update 'README.Rmd' to include installation instructions.
 #> ✔ Writing '.git/hooks/pre-commit'.
 
+# make the README.md file with plots and examples
 devtools::build_readme()
+
+# --------------- FINAL PRODUCT ------------------------------
